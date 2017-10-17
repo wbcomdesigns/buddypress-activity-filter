dc46b8bd7877f92caba16a63564d8a0a2af0ee78
Pallavi Gupta <pallavi@wbcomdesigns.com>
2017-10-17 11:32:03 +0530

    Added filters for content display of custom post type.

--- admin/class-bp-activity-filter-add-post-support.php
+++ admin/class-bp-activity-filter-add-post-support.php
@@ -63,7 +63,7 @@ if (!class_exists('WbCom_BP_Activity_Filter_Add_Post_Type_Support')) {
 					            $post_action = "";
 					            $post_action = apply_filters('bpaf_groups_content_override', $post_author, $post_type_rename, $post_title, $action );
 					            if (empty($post_action)) {
-					            	$post_action = "$post_author.' added a new '.$post_type_rename.', '.$post_title.' in the group '.$action.' about'";
+					            	$post_action = "$post_author".' added a new '."$post_type_rename".', '."$post_title".' in the group '."$action".' about';
 					            }
 
 					            $prep_args = array(
@@ -98,7 +98,7 @@ if (!class_exists('WbCom_BP_Activity_Filter_Add_Post_Type_Support')) {
 			            $post_action = "";
 			            $post_action = apply_filters('bpaf_main_activity_content_override', $post_author, $post_type_rename, $post_title);
 			            if (empty($post_action)) {
-			            	$post_action = "$post_author.' added a new '.$post_type_rename.', '.$post_title.' about'";
+			            	$post_action = "$post_author".' added a new '."$post_type_rename".', '."$post_title".' about';
 			            }
 			            // add activity
 			            $prep_args = array(
