f07a70e02918aab0f2a19f2581d3e977a0193a60
Varun Kumar Dubey <vapvarun@gmail.com>
2018-04-16 19:24:33 +0530

    Update buddypress-activity-filter.php

--- buddypress-activity-filter.php
+++ buddypress-activity-filter.php
@@ -5,7 +5,7 @@
   Plugin Name: BuddyPress Activity Filter
   Plugin URI: https://wbcomdesigns.com/downloads/buddypress-activity-filter/
   Description: Admin can set default and customized activities to be listed on front-end
-  Version: 1.0.3
+  Version: 1.0.4
   Text Domain: bp-activity-filter
   Author: Wbcom Designs<admin@wbcomdesigns.com>
   Author URI: https://www.wbcomdesigns.com/
