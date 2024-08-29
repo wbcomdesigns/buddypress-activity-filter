97c6b11674feaf5609b62e3e9451f5b29e9ffebc
vapvarun <vapvarun@gmail.com>
2024-08-29 15:27:32 +0530

    FILTER_SANITIZE_STRING Deprecation: The use of FILTER_SANITIZE_STRING has been deprecated in PHP 8.1. It’s better to rely on sanitize_text_field() for sanitizing text inputs, which is specifically designed for WordPress and ensures the input is safe.

--- admin/class-bp-activity-filter-admin-setting-save.php
+++ admin/class-bp-activity-filter-admin-setting-save.php
@@ -5,12 +5,12 @@
  * @package BuddyPress_Activity_Filter
  */
 
-if ( ! class_exists( 'WbCom_BP_Activity_Filter_Admin_Setting_Save' ) ) {
+ if ( ! class_exists( 'WbCom_BP_Activity_Filter_Admin_Setting_Save' ) ) {
 
 	/**
-	 * Defining class for Filter dropdown option for public setting
+	 * Class WbCom_BP_Activity_Filter_Admin_Setting_Save
 	 *
-	 * @package BuddyPress_Activity_Filter
+	 * Manages the saving of BuddyPress activity filter settings in the admin.
 	 */
 	class WbCom_BP_Activity_Filter_Admin_Setting_Save {
 
@@ -18,21 +18,16 @@ if ( ! class_exists( 'WbCom_BP_Activity_Filter_Admin_Setting_Save' ) ) {
 		 * Constructor.
 		 */
 		public function __construct() {
-
-			/**
-			 * Saving option values
-			 */
-
-			add_action( 'bp_admin_init', array( $this, 'bp_core_acivity_filter_admin_settings_save' ), 10 );
-
+			// Hook to save admin settings.
+			add_action( 'bp_admin_init', array( $this, 'bp_core_activity_filter_admin_settings_save' ), 10 );
 		}
 
 		/**
-		 * Saving options
+		 * Save the admin settings for activity filters.
 		 */
-		public function bp_core_acivity_filter_admin_settings_save() {
+		public function bp_core_activity_filter_admin_settings_save() {
 
-			if ( isset( $_GET['page'] ) && 'bp_activity_filter_settings' == $_GET['page'] && ! empty( $_POST['submit'] ) ) {
+			if ( isset( $_GET['page'] ) && 'bp_activity_filter_settings' === $_GET['page'] && ! empty( $_POST['submit'] ) ) {
 
 				check_admin_referer( 'buddypress-options' );
 
@@ -40,36 +35,29 @@ if ( ! class_exists( 'WbCom_BP_Activity_Filter_Admin_Setting_Save' ) ) {
 				$hidden_profile_filters = array();
 
 				if ( ! empty( $_POST['bp-default-filter-name'] ) ) {
-
 					$bp_default_filter_name = sanitize_text_field( wp_unslash( $_POST['bp-default-filter-name'] ) );
-
-					bp_update_option( 'bp-default-filter-name', filter_var( $bp_default_filter_name, FILTER_SANITIZE_STRING ) );
-
+					bp_update_option( 'bp-default-filter-name', $bp_default_filter_name );
 				}
 
 				if ( isset( $_POST['bp-hidden-filters-name'] ) && is_array( $_POST['bp-hidden-filters-name'] ) ) {
-
 					$hidden_filters = array_map( 'sanitize_text_field', wp_unslash( $_POST['bp-hidden-filters-name'] ) );
+					bp_update_option( 'bp-hidden-filters-name', $hidden_filters );
 				}
 
-				bp_update_option( 'bp-hidden-filters-name', $hidden_filters );
-
 				if ( ! empty( $_POST['bp-default-profile-filter-name'] ) ) {
-
 					$bp_default_profile_filter_name = sanitize_text_field( wp_unslash( $_POST['bp-default-profile-filter-name'] ) );
-
-					bp_update_option( 'bp-default-profile-filter-name', filter_var( $bp_default_profile_filter_name, FILTER_SANITIZE_STRING ) );
-
+					bp_update_option( 'bp-default-profile-filter-name', $bp_default_profile_filter_name );
 				}
 			}
 
 		}
 
-
 	}
 
 }
 
+// Instantiate the class if it exists.
 if ( class_exists( 'WbCom_BP_Activity_Filter_Admin_Setting_Save' ) ) {
 	$admin_setting_save_obj = new WbCom_BP_Activity_Filter_Admin_Setting_Save();
 }
+
