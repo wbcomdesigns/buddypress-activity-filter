0ae88563f37167f027945ccd1e289dd9517391a4
Wbcom Dev <pallavi@wbcomdesigns.com>
2024-10-03 13:58:17 +0530

    Fixed issue in filter activities
    https://3.basecamp.com/5798509/buckets/37595485/card_tables/cards/7876461353

--- templates/js/buddypress-activity-filter-public.js
+++ templates/js/buddypress-activity-filter-public.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
   'use strict';
 
   // Function to detect if we're on the "just-me" tab in the profile activity
@@ -14,42 +14,30 @@
   // Function to set the default filter only for the "just-me" tab or sitewide activity
   function setDefaultFilter() {
     if (isJustMeTab() || isSitewideActivity()) {
-      // Check if the filter is set via cookie
+      // Check if the filter is set via cookie      
       var userFilter = $.cookie("bpaf-default-filter");
-
-      // If no cookie is found, use the default filter from the backend (bpaf_js_object.default_filter)
       var selectedFilter = userFilter || bpaf_js_object.default_filter;
-
-      // Set the default filter in the dropdown if a valid filter is found
       if (selectedFilter) {
-        var defaultFilterOption = $("#activity-filter-by option[value='" + selectedFilter + "']");
-        if (defaultFilterOption.length) {
-          defaultFilterOption.prop('selected', true);
-        }
+        $("#activity-filter-by option[value='" + selectedFilter + "']").prop('selected', true);
       }
     }
-    // For all other tabs, skip applying the default filter
+
   }
 
   // Function to handle filter changes dynamically and update the activity stream
   function handleFilterChange() {
-    $('#activity-filter-by').on('change', function() {
-      var selectedFilter = $(this).val();
+    // Update the cookie with the newly selected filter
+    $.cookie("bpaf-default-filter", "1", {
+      path: '/'
+    });
 
-      // Update the cookie with the newly selected filter
-      $.cookie("bpaf-default-filter", selectedFilter, { path: '/' });
+    // Select the default filter option in the dropdown
+    $("#activity-filter-by option[value='" + bpaf_js_object.default_filter + "']").prop('selected', true);
 
-      // Prepare AJAX data for updating the activity stream
-      var ajaxData = {
-        action: 'activity_get_activities',
-        cookie: bp_get_cookies(),
-        scope: selectedFilter
-      };
-
-      // Update the activity stream
-      $.post(bp_ajax_url, ajaxData, function(response) {
-        $('#activity-stream').html(response.contents);
-        $(document).trigger('bp-ajax-activity-loaded');
+    // Clear the cookie when the activity filter dropdown is clicked
+    $('#activity-filter-by').on('click', function () {
+      $.removeCookie("bpaf-default-filter", {
+        path: '/'
       });
     });
   }
