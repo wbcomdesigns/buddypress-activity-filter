87e95ddfb54768727a4cd043880d45db51678e7f
Varun Kumar Dubey <vapvarun@gmail.com>
2025-06-30 23:38:17 +0530

    Squashed commit of the following:
    commit d271a0cebd9f20581bb72d99254f04feefaf545f
    Author: Nitin Patil <pallavi@wbcomdesigns.com>
    Date:   Fri Apr 11 17:24:34 2025 +0530
    
        Remove composer.lock
    
        https://3.basecamp.com/5798509/buckets/37595485/card_tables/cards/8535500697
    
    commit c760d00310f10cd195e00441d4eabf2bf0ac4858
    Author: Nitin Patil <pallavi@wbcomdesigns.com>
    Date:   Fri Apr 11 17:23:37 2025 +0530
    
        Remove unwanted files and code of wbcom folder
    
        https://3.basecamp.com/5798509/buckets/37595485/card_tables/cards/8535500697

--- admin/class-bp-activity-filter-admin-setting.php
+++ admin/class-bp-activity-filter-admin-setting.php
@@ -59,9 +59,6 @@ if (!class_exists('WbCom_BP_Activity_Filter_Admin_Setting')) {
 			<div class="wrap">
 				<div class="wbcom-bb-plugins-offer-wrapper">
 					<div id="wb_admin_logo">
-						<a href="https://wbcomdesigns.com/downloads/buddypress-community-bundle/?utm_source=pluginoffernotice&utm_medium=community_banner" target="_blank">
-							<img src="<?php echo esc_url(BP_ACTIVITY_FILTER_PLUGIN_URL) . 'admin/wbcom/assets/imgs/wbcom-offer-notice.png'; ?>">
-						</a>
 					</div>
 				</div>
 				<div class="wbcom-wrap">
